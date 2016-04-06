ea87ca9ff00ce0bde116d81430519fc952e6f235
Samantha Guergenenov <samguergen@gmail.com>
2016-04-06 11:44:20 -0400

    application views and state shell for routing

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -5,6 +5,20 @@ angular
         'templates'
     ])
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    });
+
+      $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+        });
+
+    // default fall back route
+    $urlRouterProvider.otherwise('/');
+
+    // enable HTML5 Mode for SEO
+    $locationProvider.html5Mode(true);
+
+  });
 
 
