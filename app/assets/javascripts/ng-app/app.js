9e8308a628dbae25a42bda7958c03474c6e1774b
Samantha Guergenenov <samguergen@gmail.com>
2016-04-06 13:23:47 -0400

    yay! nested views returned

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -11,7 +11,28 @@ angular
             url: '/',
             templateUrl: 'home.html',
             controller: 'HomeCtrl'
-        });
+        })
+
+        .state('dashboard', {
+            abstract: true,
+            url: '/dashboard',
+            templateUrl: 'dashboard/layout.html'
+        })
+            // the default route when someone hits dashboard
+            .state('dashboard.one', {
+                url: '',
+                templateUrl: 'dashboard/one.html'
+            })
+            // this is /dashboard/two
+            .state('dashboard.two', {
+                url: '/two',
+                templateUrl: 'dashboard/two.html'
+            })
+            // this is /dashboard/three
+            .state('dashboard.three', {
+                url: '/three',
+                templateUrl: 'dashboard/three.html'
+            });
 
 
     // default fall back route
