afa50a5f33c26fb2e7964da235134c86b8299050
Samantha Guergenenov <samguergen@gmail.com>
2016-04-06 11:25:44 -0400

    pasted ng app module decl- TODO: paste your own

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,8 @@
+angular
+    .module('myApp', [
+        'ngAnimate',
+        'ui.router',
+        'templates'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    });
