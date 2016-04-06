3cbb8e29fa86572800b3d2f6f5b930a17b9926f6
Samantha Guergenenov <samguergen@gmail.com>
2016-04-06 19:32:32 -0400

    csv to array

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -15,28 +15,28 @@ angular.module('myApp')
         ];
 
         function csvToArray(stringData, stringDelim) {
-    stringDelim = (stringDelim || ",");
-    var objPattern = new RegExp((
-    "(\\" + stringDelim + "|\\r?\\n|\\r|^)" +
-    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-    "([^\"\\" + stringDelim + "\\r\\n]*))"), "gi");
-    var arr = [[]];
-    var arrMatches = null;
-    while (arrMatches = objPattern.exec(stringData)) {
-        var stringMatchDelim = arrMatches[1];
-        if (stringMatchDelim.length && (stringMatchDelim != stringDelim)) {
-            arr.push([]);
-        }
-        if (arrMatches[2]) {
-            var stringMatchVal = arrMatches[2].replace(
-            new RegExp("\"\"", "g"), "\"");
-        } else {
-            var stringMatchVal = arrMatches[3];
-        }
-        arr[arr.length - 1].push(stringMatchVal);
-    }
-    console.log(arr);
-    return (arr);
+          stringDelim = (stringDelim || ",");
+          var objPattern = new RegExp((
+          "(\\" + stringDelim + "|\\r?\\n|\\r|^)" +
+          "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+          "([^\"\\" + stringDelim + "\\r\\n]*))"), "gi");
+          var arr = [[]];
+          var arrMatches = null;
+          while (arrMatches = objPattern.exec(stringData)) {
+              var stringMatchDelim = arrMatches[1];
+              if (stringMatchDelim.length && (stringMatchDelim != stringDelim)) {
+                  arr.push([]);
+              }
+              if (arrMatches[2]) {
+                  var stringMatchVal = arrMatches[2].replace(
+                  new RegExp("\"\"", "g"), "\"");
+              } else {
+                  var stringMatchVal = arrMatches[3];
+              }
+              arr[arr.length - 1].push(stringMatchVal);
+          }
+          console.log(arr);
+          return (arr);
 }
 
 
