94b900bbe4194c17f1bc4ae920bda80a3eeda308
Samantha Guergenenov <samguergen@gmail.com>
2016-04-07 12:47:49 -0400

    finally reading csv file

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -2,7 +2,7 @@ angular
     .module('myApp', [
         'ngAnimate',
         'ui.router',
-        'templates'
+        'templates',
     ])
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
@@ -70,35 +70,59 @@ angular
   }
  }])
 
-  .service('CSVConverterService', function () {
+  .directive('fileReader', function() {
+  return {
+    scope: {
+      fileReader:"="
+    },
+    link: function(scope, element) {
+      $(element).on('change', function(changeEvent) {
+        var files = changeEvent.target.files;
+        if (files.length) {
+          var r = new FileReader();
+          r.onload = function(e) {
+              var contents = e.target.result;
+              scope.$apply(function () {
+                scope.fileReader = contents;
+              });
+          };
 
-    this.ex = function() {
-        alert('working');
+          r.readAsText(files[0]);
+        }
+      });
     }
-
-    this.convertToArray = function (csvString) {
-        alert(csvString);
-           // Your parser logic here or call to the third party
-    };
-
-    //var csv is the CSV file with headers
-    this.csvJSON = function(csv) {
-      var lines=csv.split("\n");
-      var result = [];
-      var headers=lines[0].split(",");
-      for(var i=1;i<lines.length;i++){
-          var obj = {};
-          var currentline=lines[i].split(",");
-          for(var j=0;j<headers.length;j++){
-              obj[headers[j]] = currentline[j];
-          }
-          result.push(obj);
-      }
-      //return result; //JavaScript object
-      return JSON.stringify(result); //JSON
-    }
-
+  };
 });
+
+//   .service('CSVConverterService', function () {
+
+//     this.ex = function() {
+//         alert('working');
+//     }
+
+//     this.convertToArray = function (csvString) {
+//         alert(csvString);
+//            // Your parser logic here or call to the third party
+//     };
+
+//     //var csv is the CSV file with headers
+//     this.csvJSON = function(csv) {
+//       var lines=csv.split("\n");
+//       var result = [];
+//       var headers=lines[0].split(",");
+//       for(var i=1;i<lines.length;i++){
+//           var obj = {};
+//           var currentline=lines[i].split(",");
+//           for(var j=0;j<headers.length;j++){
+//               obj[headers[j]] = currentline[j];
+//           }
+//           result.push(obj);
+//       }
+//       //return result; //JavaScript object
+//       return JSON.stringify(result); //JSON
+//     }
+
+// });
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  root 'application#index'
+  root 'application#index', anchor: false
   get '*path' => 'application#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
