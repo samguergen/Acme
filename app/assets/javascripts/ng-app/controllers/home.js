c99d66261296eb764a8cac29a7a9fd616cb5cdcf
Samantha Guergenenov <samguergen@gmail.com>
2016-04-07 13:15:35 -0400

    displaying content as array and string

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -6,32 +6,32 @@ angular.module('myApp')
       }
 
       $scope.CSVToArray = function(strData, strDelimiter) {
-    strDelimiter = (strDelimiter || ",");
-    var objPattern = new RegExp((
-    "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-    "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
-    var arrData = [[]];
-    var arrMatches = null;
-    while (arrMatches = objPattern.exec(strData)) {
-        var strMatchedDelimiter = arrMatches[1];
-        if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
-            arrData.push([]);
+        strDelimiter = (strDelimiter || ",");
+        var objPattern = new RegExp((
+        "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+        "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+        "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+        var arrData = [[]];
+        var arrMatches = null;
+        while (arrMatches = objPattern.exec(strData)) {
+            var strMatchedDelimiter = arrMatches[1];
+            if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
+                arrData.push([]);
+            }
+            if (arrMatches[2]) {
+                var strMatchedValue = arrMatches[2].replace(
+                new RegExp("\"\"", "g"), "\"");
+            } else {
+                var strMatchedValue = arrMatches[3];
+            }
+            arrData[arrData.length - 1].push(strMatchedValue);
         }
-        if (arrMatches[2]) {
-            var strMatchedValue = arrMatches[2].replace(
-            new RegExp("\"\"", "g"), "\"");
-        } else {
-            var strMatchedValue = arrMatches[3];
-        }
-        arrData[arrData.length - 1].push(strMatchedValue);
-    }
-    console.log('the string of arrData is ', arrData.toString());
-    console.log(' arrdata is ', Array.isArray(arrData));
-    $scope.strData = arrData.toString();
-    alert(arrData);
-    return ($scope.strData);
-}
+        console.log('the string of arrData is ', arrData.toString());
+        console.log(' arrdata is ', Array.isArray(arrData));
+        $scope.arrayData = arrData;
+        $scope.stringData = arrData.toString();
+        return ($scope.arrayData);
+    };
 
     });
 
