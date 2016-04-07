f4977372b95d87ad10a7438841f432833d0bb442
Samantha Guergenenov <samguergen@gmail.com>
2016-04-07 13:12:04 -0400

    removed comms from csvtoarray func

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,11 +1,37 @@
 angular.module('myApp')
     .controller('HomeCtrl', function ($scope, $http) {
 
-      $scope.theFile = [];
-
       $scope.readCsv = function() {
         alert($scope.fileContent);
       }
+
+      $scope.CSVToArray = function(strData, strDelimiter) {
+    strDelimiter = (strDelimiter || ",");
+    var objPattern = new RegExp((
+    "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+    "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+    var arrData = [[]];
+    var arrMatches = null;
+    while (arrMatches = objPattern.exec(strData)) {
+        var strMatchedDelimiter = arrMatches[1];
+        if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
+            arrData.push([]);
+        }
+        if (arrMatches[2]) {
+            var strMatchedValue = arrMatches[2].replace(
+            new RegExp("\"\"", "g"), "\"");
+        } else {
+            var strMatchedValue = arrMatches[3];
+        }
+        arrData[arrData.length - 1].push(strMatchedValue);
+    }
+    console.log('the string of arrData is ', arrData.toString());
+    console.log(' arrdata is ', Array.isArray(arrData));
+    $scope.strData = arrData.toString();
+    alert(arrData);
+    return ($scope.strData);
+}
 
     });
 
