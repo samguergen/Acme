8ad05c0a37c17d499ca7a832517465b6877c6ec5
Samantha Guergenenov <samguergen@gmail.com>
2016-04-07 18:48:41 -0400

    yayyyy email valid working

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -131,11 +131,11 @@ angular.module('myApp')
         var email = rowArray[parseInt(emailIndex)];
         console.log(email);
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-        // if (email == '' || !re.test(email) {
-        //    // passing4.push(rowArray);
-        //    console.log('miracle');
-        // };
+            // return re.test(email);
+        if (re.test(email)) {
+           passing4.push(rowArray);
+           console.log('miracle');
+        };
       };
     };
 
