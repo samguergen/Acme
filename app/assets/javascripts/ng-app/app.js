68f5946816ed4a6bdd873255ba6f4e30b7a318ed
Samantha Guergenenov <samguergen@gmail.com>
2016-04-08 16:07:59 -0400

    responding with directives

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -53,6 +53,47 @@ angular
 
   })
 
+  .directive('fileChange',['$parse', function($parse){
+  return{
+    require:'ngModel',
+    restrict:'A',
+    controller: 'HomeCtrl',
+    link:function($scope,element,attrs,ngModel){
+      var attrHandler=$parse(attrs['fileChange']);
+      var handler=function(e){
+        $scope.$apply(function(){
+          attrHandler($scope,{$event:e,files:e.target.files});
+        });
+      };
+      element[0].addEventListener('change',handler,false);
+    }
+  }
+ }])
+
+  .directive('fileReader', function() {
+  return {
+    scope: {
+      fileReader:"="
+    },
+    link: function(scope, element) {
+      $(element).on('change', function(changeEvent) {
+        var files = changeEvent.target.files;
+        if (files.length) {
+          var r = new FileReader();
+          r.onload = function(e) {
+              var contents = e.target.result;
+              scope.$apply(function () {
+                scope.fileReader = contents;
+              });
+          };
+
+          r.readAsText(files[0]);
+        }
+      });
+    }
+  };
+})
+
 .service('Validations', function () {
 
   this.test = function(){
