d73469e5dd10a22af387a172e5a86907592ce98d
Samantha Guergenenov <samguergen@gmail.com>
2016-04-08 14:38:35 -0400

    removed unnecessary directives

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -51,78 +51,7 @@ angular
     // enable HTML5 Mode for SEO
     $locationProvider.html5Mode(true);
 
-  })
-
-  .directive('fileChange',['$parse', function($parse){
-  return{
-    require:'ngModel',
-    restrict:'A',
-    controller: 'HomeCtrl',
-    link:function($scope,element,attrs,ngModel){
-      var attrHandler=$parse(attrs['fileChange']);
-      var handler=function(e){
-        $scope.$apply(function(){
-          attrHandler($scope,{$event:e,files:e.target.files});
-        });
-      };
-      element[0].addEventListener('change',handler,false);
-    }
-  }
- }])
-
-  .directive('fileReader', function() {
-  return {
-    scope: {
-      fileReader:"="
-    },
-    link: function(scope, element) {
-      $(element).on('change', function(changeEvent) {
-        var files = changeEvent.target.files;
-        if (files.length) {
-          var r = new FileReader();
-          r.onload = function(e) {
-              var contents = e.target.result;
-              scope.$apply(function () {
-                scope.fileReader = contents;
-              });
-          };
-
-          r.readAsText(files[0]);
-        }
-      });
-    }
-  };
-});
-
-//   .service('CSVConverterService', function () {
-
-//     this.ex = function() {
-//         alert('working');
-//     }
-
-//     this.convertToArray = function (csvString) {
-//         alert(csvString);
-//            // Your parser logic here or call to the third party
-//     };
-
-//     //var csv is the CSV file with headers
-//     this.csvJSON = function(csv) {
-//       var lines=csv.split("\n");
-//       var result = [];
-//       var headers=lines[0].split(",");
-//       for(var i=1;i<lines.length;i++){
-//           var obj = {};
-//           var currentline=lines[i].split(",");
-//           for(var j=0;j<headers.length;j++){
-//               obj[headers[j]] = currentline[j];
-//           }
-//           result.push(obj);
-//       }
-//       //return result; //JavaScript object
-//       return JSON.stringify(result); //JSON
-//     }
-
-// });
+  });
 
 
 
