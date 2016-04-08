1b3d1b17151416554960ee3b6a329e32198a54a7
Samantha Guergenenov <samguergen@gmail.com>
2016-04-08 14:16:57 -0400

    all displaying in each page again but must json each obj within, not whole

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -291,6 +291,12 @@ angular.module('myApp')
     console.log('type of all orders', typeof($scope.allOrders), 'each', typeof($scope.allOrders[1]));
     console.log('type of valid orders', typeof($scope.validOrders), 'each', typeof($scope.validOrders[1]));
     console.log('type of rejected orders', typeof($scope.nonValidOrders), 'each', typeof($scope.nonValidOrders[1]));
+    console.log('and now..');
+    $scope.allOrdersJson = angular.toJson($scope.allOrders);
+    $scope.validOrdersJson = angular.toJson($scope.validOrders);
+    $scope.invalidOrdersJson = angular.toJson($scope.invalidOrders);
+    console.log(angular.toJson($scope.allOrders));
+
   };
 
  };
