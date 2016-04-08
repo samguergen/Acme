ca6e28fb481cf7b9aca99f382e297d0a41b3ad79
Samantha Guergenenov <samguergen@gmail.com>
2016-04-08 17:15:29 -0400

    directive and services added

---
+++ app/assets/javascripts/ng-app/directives/homeDirective.js
@@ -0,0 +1,42 @@
+angular.module('myApp')
+.directive('fileChange',['$parse', function($parse){
+  return{
+    require:'ngModel',
+    restrict:'A',
+    controller: 'HomeCtrl',
+    link:function($scope,element,attrs,ngModel){
+      var attrHandler=$parse(attrs['fileChange']);
+      var handler=function(e){
+        $scope.$apply(function(){
+          attrHandler($scope,{$event:e,files:e.target.files});
+        });
+      };
+      element[0].addEventListener('change',handler,false);
+    }
+  }
+ }]);
+
+angular.module('myApp')
+.directive('fileReader', function() {
+  return {
+    scope: {
+      fileReader:"="
+    },
+    link: function(scope, element) {
+      $(element).on('change', function(changeEvent) {
+        var files = changeEvent.target.files;
+        if (files.length) {
+          var r = new FileReader();
+          r.onload = function(e) {
+              var contents = e.target.result;
+              scope.$apply(function () {
+                scope.fileReader = contents;
+              });
+          };
+
+          r.readAsText(files[0]);
+        }
+      });
+    }
+  };
+});
