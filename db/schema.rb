dc41ee6c8a56536109b04e8f5a7d1ab19ffb155f
Samantha Guergenenov <samguergen@gmail.com>
2016-04-09 13:31:36 -0400

    db recreated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409171028) do
+ActiveRecord::Schema.define(version: 20160409172603) do
+
+  create_table "imports", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "order_id"
