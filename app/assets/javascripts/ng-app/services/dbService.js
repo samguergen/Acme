55e4bfd9ee700265a5e4a4d609ad1fe00e663f34
Samantha Guergenenov <samguergen@gmail.com>
2016-04-09 10:38:46 -0400

    json parse not working, perhaps because already json

--- app/assets/javascripts/ng-app/services/dbService.js
+++ app/assets/javascripts/ng-app/services/dbService.js
@@ -3,17 +3,36 @@
 angular.module('myApp')
 .service('ToServer', function ($http) {
     this.saveToDB = function(ordersJson) {
+        console.log('orders in json are ', ordersJson);
+        //double checks that the order is correct JSON format
+        // var ordersValidJson = JSON.parse(ordersJson);
+
+        // console.log('valid json now ', JSON.parse(ordersJson));
+
+      // $http({
+      //   url: 'http://localhost:3000/orders',
+      //   dataType: 'json',
+      //   method: 'POST',
+      //   data: {
+      //       orders: ordersJson,
+      //   },
+      //   headers: {
+      //       "Content-Type": "application/json"
+      //   }
+
+      //   }).success(function(response){
+      //       console.log('response is ', response);
+      //   }).error(function(error){
+      //       console.log('error is ', error);
+      //   });
+
+//without json format req
       $http({
         url: 'http://localhost:3000/orders',
-        dataType: 'json',
         method: 'POST',
         data: {
             orders: ordersJson,
-        },
-        headers: {
-            "Content-Type": "application/json"
         }
-
         }).success(function(response){
             console.log('response is ', response);
         }).error(function(error){
