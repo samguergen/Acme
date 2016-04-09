b318d46824e9190337075b073e7913c18398fd69
Samantha Guergenenov <samguergen@gmail.com>
2016-04-09 10:04:44 -0400

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160409140256) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "name"
+    t.string   "email"
+    t.string   "state"
+    t.string   "birthday"
+    t.integer  "zipcode"
+    t.boolean  "valid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
